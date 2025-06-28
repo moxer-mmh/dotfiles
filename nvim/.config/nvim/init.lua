94cc745d9b74bcf21c8720d768b98b4c7799ddd9
moxer-mmh <monefho10@gmail.com>
2025-06-28 21:29:13 +0100

    i created a neovim initial configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,1 @@
--- Entry point for Neovim configuration
--- This file bootstraps the entire configuration
-
--- Set leader keys early (before any plugins load)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Bootstrap Lazy.nvim plugin manager
-require("core.lazy")
-
--- Load core configuration modules
 require("config")
