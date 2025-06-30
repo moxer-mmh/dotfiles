fb5239399a933570d733fd9436b633744c79b87b
moxer-mmh <moncefho10@gmail.com>
2025-06-30 13:52:15 +0100

    i edited the cyberdream color scheme

--- nvim/.config/nvim/lua/plugins/cyberdream.lua
+++ nvim/.config/nvim/lua/plugins/cyberdream.lua
@@ -1,9 +1,26 @@
-return	{
+return {
   "scottmckendry/cyberdream.nvim",
   lazy = false,
   name = "cyberdream",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "cyberdream"
-  end
+    require("cyberdream").setup({
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        sidebars = "dark",
+        floats = "dark",
+      },
+      sidebars = { "qf", "help" },
+      day_brightness = 0.3,
+      hide_inactive_statusline = false,
+      dim_inactive = false,
+      lualine_bold = false,
+    })
+    vim.cmd.colorscheme("cyberdream")
+  end,
 }
