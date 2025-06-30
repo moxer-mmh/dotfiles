ea6f344620647efc25a2a126e6d28b2ba4345478
moxer-mmh <moncefho10@gmail.com>
2025-06-30 16:23:44 +0100

    i added some options and edited the colo scheme color comment also added starting auto commands and some keymaps

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,4 +4,42 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set smartindent")
 
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+vim.cmd("set cursorline")
+vim.cmd("set nowrap")
+vim.cmd("set scrolloff=8")
+vim.cmd("set sidescrolloff=8")
+vim.cmd("set signcolumn=yes")
+vim.cmd("set termguicolors")
+
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+vim.cmd("set hlsearch")
+vim.cmd("set incsearch")
+
+vim.cmd("set noswapfile")
+vim.cmd("set nobackup")
+vim.cmd("set nowritebackup")
+vim.cmd("set undofile")
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+
+
+vim.cmd("set updatetime=300")
+vim.cmd("set timeoutlen=500")
+vim.cmd("set mouse=a")
+vim.cmd("set clipboard=unnamedplus")
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.cmd("set completeopt=menuone,noselect")
+vim.cmd("set pumheight=10")
+
+vim.cmd("set lazyredraw")
+vim.cmd("set hidden")
+
+vim.cmd("set wildmenu")
